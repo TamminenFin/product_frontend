f2a5f29ce156f3ac42ce55feba1f5865c336a525
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2025-01-21 09:29:25 +0600

    next 3

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,7 @@
-import ProductsList from "@/components/MainLayout/Products";
-
 export default function Home() {
   return (
     <div className="container mx-auto px-1.5 md:px-4">
-      <div className="mt-4">
-        <ProductsList />
-      </div>
+      <div className="mt-4"></div>
     </div>
   );
 }
