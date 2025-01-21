6eb4564514b7c834c86b6ef317d1ef97caf4783f
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2025-01-21 09:39:42 +0600

    next 3

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return config;
   },
   images: {
-    domains: ["localhost", "via.placeholder.com", "res.cloudinary.com"],
+    domains: ["res.cloudinary.com"],
   },
   typescript: {
     ignoreBuildErrors: true,
